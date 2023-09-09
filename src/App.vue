dc606570b69074db05e2256cf53781c0f21910ef
inseok1121 <inseck1121@gmail.com>
2023-09-09 23:15:31 +0900

    전체적인 layout 세팅 및 card 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <mobile-menu app/>
+    <top-nav-bar app/>
+    <dashboard-view app/>
+    <content-footer app/>  
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopNavBar from './components/core/TopNavBar.vue'
+import DashboardView from './components/core/DashboardView.vue'
+import ContentFooter from './components/core/ContentFooter.vue'
+import MobileMenu from './components/core/MobileMenu.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    TopNavBar,
+    DashboardView,
+    ContentFooter,
+    MobileMenu
   },
 
   data: () => ({
---
+++ src/components/core/DashboardView.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-main>
+    <v-container fruid grid-list-md>
+      <v-layout wrap row>
+        <v-col xs12 sm6 md3 class="pb-2" v-for="i in 4" :key="i">
+          <v-card>
+            <v-container pa-1>
+              <v-layout row>
+                <v-col xs7>
+                  <v-card-title primay-title>
+                    <div>
+                      <div class="grey--text">전체 사용자</div>
+                      <h3 class="headline">123</h3>
+                    </div>
+                  </v-card-title>
+                </v-col>
+                <v-col xs5>
+                  <v-card-title primay-title>
+                    <v-avatar>
+                      <v-icon :icon="`mdiSvg:${mdiAccount}`"></v-icon>
+                    </v-avatar>
+                  </v-card-title>
+                </v-col>
+              </v-layout>
+              <v-divider light></v-divider>
+              <v-card-actions class="pa-2">
+                <v-icon small color="primary">update</v-icon>
+                <span class="grey--text caption font-italic">&nbsp;5분전</span>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-layout>
+      <v-layout wrap row>
+        <v-col xs12 sm4 class="pb-2" v-for="i in 3" :key="i">
+          <v-card height="140px" :color="`#${i}12345`">
+            간단한 차트
+          </v-card>
+        </v-col>
+      </v-layout>
+      <v-layout wrap row>
+        <v-col xs12 sm4 class="pb-2" v-for="i in 2" :key="i">
+          <v-card height="290px" :color="`#${i}54321`">
+            간단한 차트
+          </v-card>
+        </v-col>
+      </v-layout>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style> 
